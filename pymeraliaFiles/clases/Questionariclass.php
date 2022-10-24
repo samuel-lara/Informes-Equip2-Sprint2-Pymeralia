007f91c7841e3eb671d41999e7f87aff187da1a6
yasirhayati01 <yasirhacm@gmail.com>
2022-10-24 16:53:10 +0200

    Canvi de nom de la class

---
+++ pymeraliaFiles/clases/Questionariclass.php
@@ -0,0 +1,52 @@
+<?php 
+class Questionari {
+    private $estat;
+    private $preguntes;
+    private $respostes;
+  // Methods
+  function construct($preguntes, $respostes, $estat) {
+    $this->preguntes = $preguntes;
+    $this->respostes = $respostes;
+    $this->estat = $estat;
+  }
+
+  public function getEstat() {
+    return $this->estat;
+  }
+
+  public function getPreguntes () {
+    return $this-> preguntes;
+  }
+
+  public function getRespostes () {
+    return $this-> respostes;
+  }
+
+  public function setEstat($estat) {
+    $this-> estat = $estat;
+  }
+
+  public function setPreguntes($preguntes) {
+    $this -> preguntes = $preguntes;
+  }
+
+  public function setRespostes($respostes) {
+    $this -> respostes = $respostes;
+  }
+
+  /** CRUD Questionari */  
+
+  private function addQuestionari(){
+  }
+  
+  private function editQuestionari(){
+  }
+  
+  private function deleteQuestionari(){
+  }
+  
+  public function showQuestionari(){
+  }
+}
+
+?>
