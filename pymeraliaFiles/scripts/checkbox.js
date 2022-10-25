dd2b0e7f6e43377cef60ecd84bf14228884ed6d3
[ivansorribes] <[ivansorribes@iesmontsia.org]>
2022-10-25 20:44:41 +0200

    Modificar El llistat Informes + script checkbox

---
+++ pymeraliaFiles/scripts/checkbox.js
@@ -0,0 +1,10 @@
+function marcar(source) {
+    checkboxes = document.getElementsByTagName('input'); //obtenemos todos los controles del tipo Input
+    for (i = 0; i < checkboxes.length; i++) //recoremos todos los controles
+    {
+        if (checkboxes[i].type == "checkbox") //solo si es un checkbox entramos
+        {
+            checkboxes[i].checked = source.checked; //si es un checkbox le damos el valor del checkbox que lo llamó (Marcar/Desmarcar Todos)
+        }
+    }
+}
