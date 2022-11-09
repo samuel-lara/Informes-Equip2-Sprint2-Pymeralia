d0720062aa87cdc265f24e90efc6850b3087e291
samuel-lara <samuellara@iesmontsia.org>
2022-11-09 19:49:11 +0100

    crear Questionari done

--- pymeraliaFiles/clases/QuestionariClass.php
+++ pymeraliaFiles/clases/QuestionariClass.php
@@ -9,49 +9,83 @@ class Questionari {
     $this->respostes = $respostes;
     $this->estat = $estat;
   }*/
-
+  
+  /**
+   * getEstat
+   *
+   * @return void
+   */
   public function getEstat() {
     return $this->estat;
   }
-
+  
+  /**
+   * getPreguntes
+   *
+   * @return void
+   */
   public function getPreguntes () {
     return $this-> preguntes;
   }
-
+  
+  /**
+   * getRespostes
+   *
+   * @return void
+   */
   public function getRespostes () {
     return $this-> respostes;
   }
-
+  
+  /**
+   * setEstat
+   *
+   * @param  mixed $estat
+   * @return void
+   */
   public function setEstat($estat) {
     $this-> estat = $estat;
   }
-
+  
+  /**
+   * setPreguntes
+   *
+   * @param  mixed $preguntes
+   * @return void
+   */
   public function setPreguntes($preguntes) {
     $this -> preguntes = $preguntes;
   }
-
+  
+  /**
+   * setRespostes
+   *
+   * @param  mixed $respostes
+   * @return void
+   */
   public function setRespostes($respostes) {
     $this -> respostes = $respostes;
   }
 
   /** CRUD Questionari */
   //private function addQuestionari()
-  public function addQuestionari(){
+  public function addQuestionari($nombreCuestionario, $representante, $empresa, $autor, $fecha){
     ///***Include del archivo que permite conectarnos a la base de datos
     include "../includes/config-connexio.php";
 
-    if(isset($_POST['Nombre-Cuestionario'])){
-      $nombreCuestionario = $_POST['Nombre-Cuestionario'];
-      $representante = $_POST['Representante'];
-      $empresa = $_POST['Empresa'];
-      $autor = $_POST['Autor'];
-      $fecha = $_POST['Fecha'];
-              
+    if(isset($_POST['Nombre-Cuestionario']) != ""){
+          
       ///*** Query que hace el Insert a la base de datos
       $query = "INSERT INTO `test_questionari`(`Nom`, `Representant`, `Empresa`, `Autor`, `Fecha`) VALUES ('$nombreCuestionario','$representante', '$empresa','$autor','$fecha')";
-              
-      $resultQueryInsert = mysqli_query($conn, $query);
 
+      //$resultQueryInsert = mysqli_query($conn, $query);
+              
+      if($conn->query($query) != ""){
+        header("Location: ../admin/Llistat-QUestionaris.php");
+      }else{
+        header("Location: ../admin/Llistat-QUestionaris.php");
+      }
+      
       ///***Include del archivo que permite desconectarnos a la base de datos
       include "../includes/config-desconnexio.php";
     }
@@ -79,6 +113,8 @@ class Questionari {
     $result = mysqli_query($conn, $sql);
 
     return $result;
+
+    header("Location: ../admin/Llistat-Preguntes.html");
 
     ///***Include del archivo que permite desconectarnos a la base de datos
     include "../includes/config-desconnexio.php";
